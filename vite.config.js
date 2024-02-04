8b165de25c3df849f224a516ae97e724e343a823
Nehemia Telusma <telusma.nehemia@gmail.com>
2024-02-04 12:48:31 +0100

    feat: playing song + display notes in grid

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+export default {
+    root: 'src/',
+    publicDir: '../static/',
+    base: './',
+    server:
+    {
+        host: true,
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env)
+    },
+    build:
+    {
+        outDir: '../dist',
+        emptyOutDir: true,
+        sourcemap: true
+    },
+}
